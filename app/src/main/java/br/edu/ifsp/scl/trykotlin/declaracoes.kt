a1afbdac277a62c2c2f01b3b4157a35799bbd9f9
saulo <sauloealves@gmail.com>
2019-09-06 20:19:59 -0300

    Variaveis e strings templates

---
+++ app/src/main/java/br/edu/ifsp/scl/trykotlin/declaracoes.kt
@@ -0,0 +1,21 @@
+package br.edu.ifsp.scl.trykotlin
+fun  main(args: Array<String>){
+    // mutaveis  -- variavles - var
+
+    var frase: String = "Hello World"
+    println(frase)
+
+    // imprimir com string template
+    println("$frase")
+
+    println("${frase}")
+
+    // imprimir com chamada de funcao
+    println("Quantidade de chars: ${frase.count()}")
+
+
+    // imprimir com propriedade (get)
+    println("Quantidade de chars: ${frase.length}")
+
+    // imutavel values - val
+}
